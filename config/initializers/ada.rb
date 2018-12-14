e26a008c0ca58493cbd36e0eec07b6ff501629a0
magwas <m4gw4s@gmail.com>
2018-12-14 20:17:16 +0100

    first stab at ADA authentications

---
+++ config/initializers/ada.rb
@@ -0,0 +1,14 @@
+
+# frozen_string_literal: true
+
+if Rails.application.secrets.dig(:omniauth, :participa, :enabled)
+  require "omniauth/strategies/ada"
+
+  Devise.setup do |config|
+    config.omniauth :ada,
+                    name: "participa",
+                    identifier: Rails.application.secrets.dig(:omniauth, :ada, :identifier)
+  end
+
+  Decidim::User.omniauth_providers << :ada
+end
